4560afa0ef5dfeb9f3f70239a59bdd4d89b8164c
Jeff Bienstadt <jeffbi@jetstreamsoftware.com>
2021-10-26 16:44:23 -0700

    Re-write of singly-linked list

---
+++ LinkedList/C++/SingleLinkedList.h
@@ -0,0 +1,292 @@
+/// \file   SingleLinkedList.h
+/// \brief  Implementation of a singly-linked list in C++
+/// \author Jeffrey K. Bienstadt
+/// \copyright  Copyright (c) 2019-2021 Jeffrey K. Bienstadt
+
+#ifndef INC_SINGLE_LINKED_LIST
+#define INC_SINGLE_LINKED_LIST
+
+/// \brief  An implementation of a singly-linked list.
+template<typename T>
+class SingleLinkedList
+{
+private:
+    struct node_t;
+
+    /// \brief  Base of the linked list node structure.
+    ///
+    /// This allows us to have a "dummy node" before the head node.
+    /// The dummy node has just a pointer to the head, there is no
+    /// additional storage needed for data contained in an actual node.
+    struct node_base
+    {
+        node_t *_next{nullptr};
+    };
+
+    /// \brief  The linked list node structure.
+    ///
+    /// This structure is the core of the linked list, holding the list element's
+    /// data and the pointer to the next node in the list.
+    ///
+    /// This structure is not exposed to the list user. Instead it is abstracted
+    /// away by the Iterator class.
+    struct node_t : public node_base
+    {
+        T   _data;
+
+        explicit node_t(const T &data)
+          : _data{data}
+        {}
+
+        node_t(const node_t &) = delete;
+        node_t & operator=(const node_t &) = delete;
+    };
+
+public:
+    /// \brief  A forward iterator that provides access to the data in a \c LinkedList.
+    ///
+    /// \remark An iterator acts like a pointer to the data in a linked list.
+    /// \remark This iterator may be used in a range-based \c for.
+    class Iterator
+    {
+    public:
+        /// \brief  Construct an \c Iterator with a \c node
+        ///
+        /// \param node The linked list node to be wrapped by this iterator.
+        explicit Iterator(node_t *node = nullptr) noexcept
+          : _node{node}
+        {}
+        Iterator(const Iterator &) = default;
+        Iterator & operator=(const Iterator &) = default;
+
+        /// \brief  Dereference the iterator, getting access to the underlying data.
+        ///
+        /// \return A reference to the data pointed to by this iterator.
+        const T & operator*() const noexcept
+        {
+            return _node->_data;
+        }
+
+        /// \brief  Use pointer-like arrow operator to access members of the underlying data.
+        ///
+        /// \return A pointer to the underlying data.
+        const T * operator->() const noexcept
+        {
+            return &_node->_data;
+        }
+
+        /// \brief  Prefix increment operator. Increments the iterator to point to
+        ///         the next node in the linked list.
+        ///
+        /// \return *this
+        Iterator & operator++() noexcept
+        {
+            _node = _node->_next;
+            return *this;
+        }
+
+        /// \brief  Postfix increment operator. Increments the iterator to point to
+        ///         the node in the linked list.
+        ///
+        /// \return An iterator pointing to the next item in the linked list.
+        Iterator operator++(int) noexcept
+        {
+            Iterator tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        /// \brief  Equality operator. Determines if this iterator is equal to another,
+        ///         i.e., if they both refer to the same node in the linked list.
+        ///
+        /// \param rhs  A refererce to a ListIterator to compare to this iterator.
+        ///
+        /// \return \c true if \p rhs is equal to this iterator, \c false otherwise.
+        bool operator==(const Iterator &rhs) const noexcept
+        {
+            return _node == rhs._node;
+        }
+
+        /// \brief  Inequality operator. Determines if this iterator is not equal
+        ///         to another, i.e., if they each do not refer to the same node
+        ///         in the linked list.
+        ///
+        /// \param rhs  A refererce to a ListIterator to compare to this iterator.
+        ///
+        /// \return \c true if \p rhs is not equal to this iterator, \c false otherwise.
+        bool operator!=(const Iterator &rhs) const noexcept
+        {
+            return !(*this == rhs);
+        }
+
+        friend SingleLinkedList;
+    private:
+        node_t *_node;
+    };
+
+
+    /// \brief  Default-construct an empty SingleLinkedList.
+    SingleLinkedList() noexcept = default;
+
+    /// \brief  Destroy a Linked List.
+    ///
+    /// Any contained nodes will be removed and their memory reclaimed.
+    ~SingleLinkedList()
+    {
+        clear();
+    }
+
+    SingleLinkedList(const SingleLinkedList &) = delete;
+    SingleLinkedList & operator=(const SingleLinkedList &) = delete;
+
+    /// \brief  Determine if a SingleLinkedList is empty.
+    ///
+    /// \return \c true \c if the SingleLinkedList is empty, \c false \c otherwise.
+    bool is_empty() const noexcept
+    {
+        return _pre_head._next == nullptr;
+    }
+
+    /// \brief  Prepend a new item to the beginning of a linked list. The new
+    ///         item becomes the new head of the linked list.
+    ///
+    /// \param data The data to be added to the linked list.
+    /// \return An iterator to the prepended item.
+    Iterator prepend(const T &data)
+    {
+        node_t *node = new node_t(data);
+
+        if (is_empty())
+        {
+            _pre_head._next = node;
+        }
+        else
+        {
+            node->_next = _pre_head._next;
+            _pre_head._next = node;
+        }
+
+        return Iterator(node);
+    }
+
+    /// \brief  Append a new item to the end of the linked list.
+    ///
+    /// \param data The data to be appended to the linked list.
+    /// \return An iterator to the appended item.
+    ///
+    /// \remark This function must locate the tail of the linked list in order
+    ///         to append the new item. This can be slow in large lists.
+    ///         Consider using the \c insert_after function when appending more
+    ///         than one item to the end of a long list.
+    Iterator append(const T &data)
+    {
+        node_t *node = new node_t(data);
+
+        if (is_empty())
+            _pre_head._next = node;
+        else
+            find_tail_node()->_next = node;
+
+        return Iterator(node);
+    }
+
+    /// \brief  Erase the linked list. Memory allocated to nodes is reclaimed.
+    void clear() noexcept
+    {
+        node_t *current = _pre_head._next;
+
+        while (current)
+        {
+            node_t *next = current->_next;
+
+            delete current;
+            current = next;
+        }
+
+        _pre_head._next = nullptr;
+    }
+
+    /// \brief  Return an iterator to the head of the linked list.
+    Iterator begin() const noexcept
+    {
+        return Iterator(_pre_head._next);
+    }
+
+    /// \brief  Return an iterator to one past the tail of the linked list.
+    Iterator end() const noexcept
+    {
+        return Iterator(nullptr);
+    }
+
+    /// \brief  Return an iteraor to the position just before the head of
+    ///         the linked list.
+    ///
+    ///         This iterator's use cases are for use with \c insert_after
+    ///         and \c remove_after. It may be incremented, but it must not
+    ///         be dereferenced either with the \c * or \c -> operators.
+    Iterator before_begin() noexcept
+    {
+        return Iterator(static_cast<node_t *>(&_pre_head));
+    }
+
+    /// \brief  Return an iterator to the last element of the linked list.
+    Iterator tail() const noexcept
+    {
+        return Iterator(find_tail_node());
+    }
+
+    /// \brief  Insert a new item into the linked list immediately following
+    ///         the specified iterator.
+    ///
+    /// \param it   An iterator pointing to an existing item in the linked list.
+    /// \param data The data to be inserted into the list.
+    ///
+    /// \return An iterator pointing to the new list item.
+    Iterator insert_after(const Iterator &it, const T &data)
+    {
+        node_t *node = it._node;
+        node_t *new_node = new node_t(data);
+
+        new_node->_next = node->_next;
+        node->_next = new_node;
+
+        return Iterator(new_node);
+    }
+
+    Iterator remove_after(const Iterator &it)
+    {
+        node_t *node = it._node;
+        node_t *rem = node->_next; // node to be removed
+
+        if (rem)
+        {
+            node->_next = rem->_next;
+            delete rem;
+        }
+
+        return Iterator(node->_next);
+    }
+private:
+    /// \brief  Helper function to find the last node in the list.
+    ///
+    /// \return A pointer to the last node in the list, or nullptr if the list
+    ///         is empty.
+    node_t *find_tail_node() const noexcept
+    {
+        if (is_empty())
+            return nullptr;
+
+        node_t *current = _pre_head._next;
+
+        while (current->_next)
+            current = current->_next;
+
+        return current;
+    }
+
+// Instance data
+private:
+    node_base  _pre_head;   // The "dummy node" before the head.
+};
+
+#endif // INC_SINGLE_LINKED_LIST
--- LinkedList/C++/sample/linked_list_sample.cpp
+++ LinkedList/C++/sample/linked_list_sample.cpp
@@ -1,12 +1,12 @@
-// Copyright (c) 2019 Jeffrey K. Bienstadt
+// Copyright (c) 2019-2021 Jeffrey K. Bienstadt
 
 #include <chrono>
 #include <iostream>
 
-#include "../LinkedList.h"
+#include "../SingleLinkedList.h"
 
 template<typename T>
-void traverse_list(LinkedList<T> &list)
+void traverse_list(T &list)
 {
     std::cout << "Traversing linked list\n";
 
@@ -20,47 +20,77 @@ void traverse_list(LinkedList<T> &list)
     std::cout << n << " nodes in the list\n";
 }
 
-int main()
+void test_singly_linked_list()
 {
-    LinkedList<int> list;
+    // We can use our linked list with whatever type we need.
+    SingleLinkedList<double> list;
+
+    struct AB
+    {
+        int a;
+        int b;
+    };
+    SingleLinkedList<AB> ab_list;
 
     traverse_list(list);
 
-    list.append(42);
+    list.append(98.6);
+    
     traverse_list(list);
 
-    auto it_b = list.begin();
+    // Append a couple of AB objects to our list of AB structures.
+    ab_list.append({42, 9});
+    ab_list.append({63, 80});
 
-    auto it = list.insert_after(it_b, 7);
-    list.insert_after(it, 314);
+
+    // Get an iterator for the first item in the list of doubles.
+    auto it_beg = list.begin();
+
+    // Print what the iterator refers to.
+    std::cout << "Iterator has " << *it_beg << '\n';
+
+    // Use an iterator to insert new elements.
+    auto it_ins = list.insert_after(it_beg, 3.14);
+    list.insert_after(it_ins, 2.71828);
+
+    // Use the before-begin iterator to insert a new element before the head node
+    list.insert_after(list.before_begin(), 1.414);
+    traverse_list(list);
+    // Also use it to remove the head node
+    list.remove_after(list.before_begin());
+    traverse_list(list);
 
     // We can use our simple iterators with the range-based for statement
+    std::cout << "From range-based for loop:\n";
     for (auto &v : list)
-    {
         std::cout << v << '\n';
-    }
 
-    list.erase();
+    // We can also manually traverse a list with iterators,
+    // and use the iterator as a pointer to access an item's members
+    std::cout << "Manual iteration:\n";
+    for (auto iter = ab_list.begin(); iter != ab_list.end(); ++iter)
+        std::cout << "member a: " << iter->a << ", member b: " << iter->b << '\n';
 
-    std::chrono::high_resolution_clock::time_point dthen;
+    list.clear();
 
-    LinkedList<int> list2;
+    SingleLinkedList<int>   int_list;
 
     std::cout << "prepending 10,000,000 elements\n";
     auto then = std::chrono::high_resolution_clock::now();
     // prepend 10 million elements to the front of the list
     for (int i=0; i < 10000000; ++i)
-        list2.prepend(i);
+        int_list.prepend(i);
     auto now = std::chrono::high_resolution_clock::now();
 
     std::cout << "prepending took "
               << std::chrono::duration_cast<std::chrono::milliseconds>(now - then).count()
               << " milliseconds\n";
 
-    std::cout << "appending just 500 elements (this can take several minutes)\n";
+
+    std::cout << "appending just 500 elements (this can take quite a while)\n";
     then = std::chrono::high_resolution_clock::now();
     for (int i=0; i < 500; ++i)
-        list2.append(i);
+        int_list.append(i);
     now = std::chrono::high_resolution_clock::now();
 
     std::cout << "appending took "
@@ -71,20 +101,27 @@ int main()
     // A faster way to append a lot of elements, using insert_after
     std::cout << "appending 10,000,000 elements using insert_after\n";
     then = std::chrono::high_resolution_clock::now();
-    it = list2.tail();
+    auto it_tail = int_list.tail();
     for (int i=0; i < 10000000; i++)
-        it = list.insert_after(it, i);
+        it_tail = int_list.insert_after(it_tail, i);
     now = std::chrono::high_resolution_clock::now();
 
     std::cout << "appending via insert_after took "
               << std::chrono::duration_cast<std::chrono::milliseconds>(now - then).count()
               << " milliseconds\n";
 
-    dthen = std::chrono::high_resolution_clock::now();
-
-    auto dnow = std::chrono::high_resolution_clock::now();
+    // erase all items in the list.
+    then = std::chrono::high_resolution_clock::now();
+    int_list.clear();
+    now = std::chrono::high_resolution_clock::now();
 
     std::cout << "destroying 20,000,500 elements took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(dnow - dthen).count()
+              << std::chrono::duration_cast<std::chrono::milliseconds>(now - then).count()
               << " milliseconds\n";
+
+}
+
+int main()
+{
+    test_singly_linked_list();
 }
